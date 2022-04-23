bac046081f4d476f62821c9c2649dbd282fa3c45
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-23 18:28:01 +0000

    fix: missing imports

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
-from brownie import network
+
+import pytest
+from brownie import chain, network
+from yearn.networks import Network
 
 network.connect('mainnet')
+
 mainnet_only = pytest.mark.skipif(
     chain.id != Network.Mainnet, reason="This test is only applicable on mainnet"
 )
