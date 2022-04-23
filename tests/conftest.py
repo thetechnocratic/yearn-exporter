6867d46d3347b081597b6e8effec37c8331b7743
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-23 18:28:01 +0000

    feat: BROWNIE_NETWORK_ID_MAINNET env for testing when using atypical brownie network id

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 
 import pytest
+import os
 from brownie import chain, network
 from yearn.networks import Network
 
-network.connect('mainnet')
+network.connect(os.environ.get('BROWNIE_NETWORK_ID_MAINNET','mainnet'))
 
 mainnet_only = pytest.mark.skipif(
     chain.id != Network.Mainnet, reason="This test is only applicable on mainnet"
