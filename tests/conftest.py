8d3c5447e6c0eebd8b151958dd6241e6d2f3fff9
BobTheBuidler <bobthebuidlerdefi@gmail.com>
2022-04-23 18:28:01 +0000

    test: endure middleware is on for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,10 @@ from yearn.networks import Network
 
 network.connect(os.environ.get('BROWNIE_NETWORK_ID_MAINNET','mainnet'))
 
+# Import must occur after brownie is connected.
+from yearn.middleware.middleware import setup_middleware
+setup_middleware()
+
 mainnet_only = pytest.mark.skipif(
     chain.id != Network.Mainnet, reason="This test is only applicable on mainnet"
 )
